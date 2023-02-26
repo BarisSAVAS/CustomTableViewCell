1372f56b6a26378128c3c69e8d482b7806595b4c
Barış Savas <44875959+BarisSAVAS@users.noreply.github.com>
2023-02-27 02:47:53 +0300

    Second Commit

--- CustomTableViewCell/CustomTableViewCell.swift
+++ CustomTableViewCell/CustomTableViewCell.swift
@@ -10,15 +10,18 @@ import SnapKit
 
 class CustomTableViewCell: UITableViewCell {
     static let identifier = "CustomTableViewCell"
+  //MARK: Cell Label olusturuldu
     let cellLabel: UILabel = {
         let cellLabel = UILabel()
         cellLabel.text = "Deneme Deneme"
-        cellLabel.font = .systemFont(ofSize: 30)
+        cellLabel.font = .systemFont(ofSize: 20)
         return cellLabel
     }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        contentView.backgroundColor = .orange
+        contentView.backgroundColor = .systemBackground
+        //MARK: Cell Label view a gomuldu
         contentView.addSubview(cellLabel)
         configure()
     }
@@ -33,12 +36,14 @@ class CustomTableViewCell: UITableViewCell {
 //                                 width: contentView.frame.width-10,
 //                                 height: contentView.frame.size.height-10)
 //    }
+    //MARK: Snapkit ile tasarim yapildi
     func configure(){
         let cellLabel = cellLabel
         cellLabel.snp.makeConstraints { make in
             make.width.equalTo(contentView.snp.width)
             make.height.equalTo(contentView.snp.height)
-            make.leading.equalTo(contentView.snp.leading).offset(20)
+            make.leading.equalTo(contentView.snp.leading).offset(10)
+            make.top.equalTo(contentView.snp.top).inset(-15)
         }
     }
 }
